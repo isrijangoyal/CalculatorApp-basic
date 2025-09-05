48a8e4e29a53332cad391a6c0a8298acf2dc2b49
isrijangoyal <goyalsrijan987@gmail.com>
2025-09-05 21:13:32 +0530

    Initial commit

---
+++ src/main/java/org/example/CalculatorApp.java
@@ -0,0 +1,73 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+// Calculator class with methods for basic operations
+class Calculator {
+    // Addition
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    // Subtraction
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    // Multiplication
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    // Division
+    public double divide(double a, double b) {
+        if (b == 0) {
+            System.out.println("Error! Division by zero is not allowed.");
+            return 0;
+        }
+        return a / b;
+    }
+}
+
+// Main class to run the calculator
+public class CalculatorApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Calculator calc = new Calculator();
+
+        System.out.println("==== Simple Calculator ====");
+        System.out.println("Choose operation: +, -, *, /");
+        char operator = sc.next().charAt(0);
+
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        double result = 0;
+
+        switch (operator) {
+            case '+':
+                result = calc.add(num1, num2);
+                break;
+            case '-':
+                result = calc.subtract(num1, num2);
+                break;
+            case '*':
+                result = calc.multiply(num1, num2);
+                break;
+            case '/':
+                result = calc.divide(num1, num2);
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                System.exit(0);
+        }
+
+        System.out.println("Result: " + result);
+        sc.close();
+    }
+}
